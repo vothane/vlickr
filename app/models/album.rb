75cec41dde79abf326e68d3c619c8d5215d175ed
Thane Vo <vothane@gmail.com>
2012-04-13 15:05:13 -0600

    added spec unit tests to model Album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,22 +1,21 @@
 class Album < ActiveRecord::Base
-  attr_accessor :name, :description
+  attr_accessor :title, :description
 
   belongs_to :user
   has_many   :videos
   
-  validates :name,  :presence => true,
+  validates :title, :presence => true,
                     :length   => { :maximum => 50 }
   
-  validates :email, :presence   => true,
-                    :uniqueness => { :case_sensitive => false }
+  validates :description, :presence   => true
                     
   after_create :create_ooyala_label
   
   private
   
   def create_ooyala_label
-    label = Lable.new
-    label.name = :name
-    label.save
+    #label = Lable.new
+    #label.name = :name
+    #label.save
   end  
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Album do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user = Factory(:user)
+    @attr = { :title => "lorem ipsum", :description => "lorem ipsum yada yada" }
+  end
+  
+  it "should create a new instance with valid attributes" do
+    @user.albums.create!(@attr)
+  end
+  
+  describe "user associations" do
+    
+    before(:each) do
+      @album = @user.albums.create(@attr)
+    end
+    
+    it "should have a user attribute" do
+      @album.should respond_to(:user)
+    end
+    
+    it "should have the right associated user" do
+      @album.user_id.should == @user.id
+      @album.user.should == @user
+    end
+  end
+  
+  describe "validations" do
+
+    it "should have a user id" do
+      album.new(@attr).should_not be_valid
+    end
+
+    it "should require nonblank content" do
+      @user.albums.build(:description => "    ").should_not be_valid
+    end
+    
+    it "should reject long content" do
+      @user.albums.build(:description => "a" * 141).should_not be_valid
+    end
+  end
 end
