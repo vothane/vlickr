d6c170362057e6529c9cdfccde9c40d6633f6dd9
Thane Vo <vothane@gmail.com>
2012-04-14 16:38:43 -0600

    some refactoring to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessor :name, :email
 
-  has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
-  has_many :followers,     :through => :relationships, :class_name => "User", :source => "user_id"
-  has_many :follows,       :through => :relationships, :class_name => "User", :source => "follower_id"
-  has_many :albums,        :dependent => :destroy
-  has_many :videos,        :through => :albums, :dependent => :destroy
-  has_many :comments,      :dependent => :destroy
+  has_many :relationships,  :foreign_key => "follower_id", :dependent => :destroy
+  has_many :follows,        :through     => :relationships, :class_name => "User", :source => "follower_id"
+  has_many :albums,         :dependent   => :destroy
+  has_many :videos,         :through     => :albums, :dependent => :destroy
+  has_many :comments,       :dependent   => :destroy
+  has_many :followed_users, :through     => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name  => "Relationship",
+                                   :dependent   => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -15,11 +19,24 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+                    
   def follow(user)
     Relationship.create(:user => user, :follower => self)
   end
 
   def unfollow(user)
     Relationship.first(:user_id => user.id, :follower_id => self.id).destroy
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id => other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 end
