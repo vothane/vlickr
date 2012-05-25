7f73ab4fc6228c4f2be09717459f96805ff5d4a8
Thane Vo <vothane@gmail.com>
2012-05-25 16:45:37 -0600

    changed explicit routes to named routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "StaticPages" do
   subject { page }
 
   describe "Home page" do
-    before { visit '/static_pages/home' }
+    before { visit root_path }
 
     it { should have_selector('h1', text: 'VLICKR') }
     it { should have_selector('title', text: '') }
@@ -13,28 +13,28 @@ describe "StaticPages" do
   end
 
   describe "Help page" do
-    before { visit '/static_pages/help' }
+    before { visit help_path }
 
     it { should have_selector('h1', text: 'Help') }
     it { should have_selector('title', text: 'Help') }
   end
 
   describe "About page" do
-    before { visit '/static_pages/about' }
+    before { visit about_path }
 
     it { should have_selector('h1', text: 'About') }
     it { should have_selector('title', text: 'About Us') }
   end
 
   describe "Contact page" do
-    before { visit '/static_pages/contact' }
+    before { visit contact_path }
 
     it { should have_selector('h1', text: 'Contact') }
     it { should have_selector('title', text: 'Contact') }
   end
 
   it "should have the right links on the layout" do
-    visit '/static_pages/home'
+    visit root_path
     click_link "About"
     page.should have_selector 'a', text: 'About'
     click_link "Help"
