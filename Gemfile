f5fd862583695931be496a5f5a1a11bb4ccfb8a1
Thane Vo <vothane@gmail.com>
2013-04-02 20:05:24 -0600

    first tests

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'jbuilder', '~> 1.0.1'
 group :development, :test do
   gem 'sqlite3'
   gem 'rspec-rails'
+  gem 'spork'
   gem 'jasmine', :git => 'git://github.com/pivotal/jasmine-gem.git'
   gem 'pry'
   gem 'awesome_print'
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Video do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when creating a new video" do
+    let(:asset_video) do
+      results = Asset.find(:one) do |vid|
+        vid.description == "Thor"
+        vid.duration > 600
+      end
+      results.first
+    end  
+
+    let(:video) do
+      video = Video.new
+      video.video = asset_video
+      video
+    end  
+
+    it "should be valid" do
+      video.should be_valid
+    end  
+  end  
 end
