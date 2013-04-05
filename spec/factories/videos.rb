a5f0b6644a54e10dbd809cdec2db9ac335a8baea
Thane Vo <vothane@gmail.com>
2013-04-05 17:18:08 -0600

    create some tests for comment model

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,9 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-require 'factory_girl'
 
 FactoryGirl.define do
   factory :comment do
-    content "MyText"
-    video_id 1
+    title "MyTiltle"
+    user_id 1
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:asset_video) { Asset.find(:one) { |vid| vid.name == "Iron Sky" } }
+
+  let(:video) do
+    video = Video.new
+    video.asset = asset_video
+    video
+  end  
+
+  before do
+    @comment = comment.new(content: "Lorem ipsum and sum", video_id: 2)
+  end
+  
+  subject { @comment }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:video_id) }
+
+  describe "when video_id is not present" do
+    before { @comment.video_id = nil }
+    it { should_not be_valid }
+  end
+  
+  describe "when video_id is not present" do
+    before { @comment.video_id = nil }
+    it { should_not be_valid }
+  end
+    
+  describe "with blank content" do
+    before { @comment.content = " " }
+    it { should_not be_valid }
+  end
+  
+  describe "with content that is too long" do
+    before { @comment.content = "a" * 141 }
+    it { should_not be_valid }
+  end
+
 end
