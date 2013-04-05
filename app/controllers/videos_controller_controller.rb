914bb2c34d5df53a98a4fa15e0a4ecebf7b12e67
Thane Vo <vothane@gmail.com>
2013-04-04 19:14:09 -0600

    include ActionController::Live and code for streaming video data

--- app/controllers/videos_controller_controller.rb
+++ app/controllers/videos_controller_controller.rb
@@ -1,17 +1,54 @@
 class VideosControllerController < ApplicationController
+  
+  include ActionController::Live
+
   def index
     @videos = Video.all
   end
 
   def create
+    attributes = params.require(:message).permit(:name)
+    response.headers["Content-Type"] = "text/javascript"
+    live_message("create")
   end
 
   def destroy
+    response.headers["Content-Type"] = "text/javascript"
+    live_message("destroy")
   end
 
   def edit
+    attributes = params.require(:message).permit(:name)
+    response.headers["Content-Type"] = "text/javascript"
+    live_message("edit")
   end
 
   def update
+    attributes = params.require(:message).permit(:name)
+    response.headers["Content-Type"] = "text/javascript"
+    live_message("update")
   end
+  
+  def events
+    response.headers["Content-Type"] = "text/event-stream"
+    redis = Redis.new
+    redis.psubscribe('messages.*') do |on|
+      on.pmessage do |pattern, event, data|
+        response.stream.write("event: #{event}\n")
+        response.stream.write("data: #{data}\n\n")
+      end
+    end
+  rescue IOError
+    logger.info "Stream closed"
+  ensure
+    redis.quit
+    response.stream.close
+  end
+
+  private
+
+  def live_message(event)
+    @message = Message.create(attributes)
+    $redis.publish("messages.#{event}", @message.to_json)
+  end  
 end
