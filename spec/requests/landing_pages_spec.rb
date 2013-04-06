bec33d9662f5d9e4a2c36c47e8ca4cd2c64d6e25
Thane Vo <vothane@gmail.com>
2013-04-06 13:46:30 -0600

    more integration tests

--- spec/requests/landing_pages_spec.rb
+++ spec/requests/landing_pages_spec.rb
@@ -1,14 +1,40 @@
 require 'spec_helper'
 
-describe "LandingPages" do
-  describe "Static pages" do
-    describe "Homepage" do
+describe "Static pages" do
 
-      it "should have the content 'Vlickr'" do
-        visit '/staticpages/home'
-        page.should have_content('Vlickr')
-      end
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('Vlickr') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+
+    describe "for signed-in users" do
       
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
     end
   end
+
+  describe "About page" do
+    before { visit about_path }
+    
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+
+
 end
