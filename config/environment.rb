46de695a0a0e35a98e2499139df63021c660158a
Thane Vo <vothane@gmail.com>
2013-04-09 20:34:03 -0600

    load ENV variables

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application.
 Vlickr::Application.initialize!
+
+# Load custom config file for current environment
+raw_config = File.read(RAILS_ROOT + "/config/config.yml")
+APP_CONFIG = YAML.load(raw_config)[RAILS_ENV]
