c67b6ef87463d5b65a5dfd0fe30f691f6539c044
Thane Vo <vothane@gmail.com>
2013-04-10 16:02:55 -0600

    ok to assign a user with a pre-existing admin approved player

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 require "copy"
 
 class User < ActiveRecord::Base 
+  serialize :player, Player
 
   has_many :albums, dependent: :destroy, :extend => Copy
   has_many :videos, dependent: :destroy, :autosave => true
 
   before_save Proc.new { |user| user.email.downcase! }
+  before_destroy Proc.new { |player| player.video.destroy }
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i  
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-                    
+
+  validates :player, :presence => true  
+  
+  delegate :player_code, :to => :player  
+
 end
