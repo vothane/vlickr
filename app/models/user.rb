2954eb5cde9674c43231a026c90c0fef70f05bfe
Thane Vo <vothane@gmail.com>
2013-04-14 17:45:10 -0600

    add Sessions helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
   before_save Proc.new { |user| user.email.downcase! }
   before_destroy Proc.new { |player| player.video.destroy }
+  before_save :create_remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i  
@@ -20,6 +21,11 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
   
-  delegate :player_code, :to => :player  
+  delegate :player_code, :to => :player 
 
+  private
+  
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end 
 end
