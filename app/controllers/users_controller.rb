5d85e71a3ddba46ad3bf4b62cc1062a232cd743f
Thane Vo <vothane@gmail.com>
2013-04-14 19:22:02 -0600

    hacks to get things to work when running rails s

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,12 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in @user
