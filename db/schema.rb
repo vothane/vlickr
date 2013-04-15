c1bb28fda0c444314a9a06bf23dbee64400a0839
Thane Vo <vothane@gmail.com>
2013-04-15 16:41:20 -0600

    add admin attribute to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130414231420) do
+ActiveRecord::Schema.define(version: 20130415223733) do
 
   create_table "albums", force: true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20130414231420) do
     t.string   "password_digest"
     t.text     "player"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
