8f54849f0097f6caccef62ff3552de832e2f5797
Thane Vo <vothane@gmail.com>
2013-04-15 16:49:16 -0600

    update Users controller with admin rights code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
   def index
     @users = User.all
@@ -63,4 +64,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end 
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 end
