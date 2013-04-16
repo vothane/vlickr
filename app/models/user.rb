5abc3e1b106132101df5525133c7e07ee368fb6f
Thane Vo <vothane@gmail.com>
2013-04-15 19:02:57 -0600

    add implementation to pass 'follow' tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
   has_many :albums, dependent: :destroy, :extend => Copy
   has_many :videos, dependent: :destroy, :autosave => true
 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   before_save Proc.new { |user| user.email.downcase! }
   before_destroy Proc.new { |player| player.video.destroy }
   before_save :create_remember_token
@@ -22,6 +27,18 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
   
   delegate :player_code, :to => :player 
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
 
   private
   
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,14 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+def random_string
+  SecureRandom.hex(3)
+end
+
 FactoryGirl.define do
   factory :user do
     sequence(:name) { |n| "Person #{n}" }
     sequence(:user_name) { |n| "User #{n}" }
-    sequence(:email) { |n| "person_#{n}@example.com"}    
+    sequence(:email) { |n| "person_#{n}#{random_string}@example.com"}    
     password "foobar"
     password_confirmation "foobar"
   end
